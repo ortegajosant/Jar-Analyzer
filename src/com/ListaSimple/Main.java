3f70af55b825721fd4910c9d254537bb0fcc5e56
AndreyGarro <diego.garro120@gmail.com>
2018-06-15 11:31:41 -0600

    Se inicia la interfaz y se puede seleccionar el jar

--- src/com/ListaSimple/Main.java
+++ src/com/ListaSimple/Main.java
@@ -1,13 +1,15 @@
+package com.ListaSimple;
+
 public class Main {
 
-	public static void main(String[] args) {
-		ListaEnlazada lista = new ListaEnlazada();
-		lista.agregarFinal(10);
-		lista.agregarFinal(2);
-		lista.agregarFinal(8);
-		lista.agregarFinal(5);
-		lista.agregarFinal(6);
-		lista.print();
+    public static void main(String[] args) {   
+        ListaEnlazada lista = new ListaEnlazada();
+	lista.agregarFinal(10);
+        lista.agregarFinal(2);
+	lista.agregarFinal(8);
+        lista.agregarFinal(5);
+	lista.agregarFinal(6);
+        lista.print();
 	}
 
 }
