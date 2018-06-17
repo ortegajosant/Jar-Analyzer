69de36b29ac2e0a3888da06202ff65f34a0d9275
AndreyGarro <diego.garro120@gmail.com>
2018-06-16 23:24:52 -0600

    Se agregan las opciones para hacer zoom in y zoom out, asi como otras mejoras en las estructuras

--- src/com/jaranalyzer/interfaz/DibujaGrafo.java
+++ src/com/jaranalyzer/interfaz/DibujaGrafo.java
@@ -1,6 +1,9 @@
 package com.jaranalyzer.interfaz;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.IOException;
@@ -9,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -28,36 +32,77 @@ import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.layout.Pane;
 
-public class DibujaGrafo {
+public class DibujaGrafo extends JFrame implements ActionListener{
 	
-	public static void DibujarGrafo(Graph g) {
+	private static DibujaGrafo instance;
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem zoomIn, zoomOut;
+	private String vertex = "";
+	
+	private DibujaGrafo() {};
+	
+	public static DibujaGrafo getInstance() {
+		if(instance == null) {
+			instance = new DibujaGrafo();
+		}
+		return instance;
+	}
+	
+	public void DibujarGrafo(Graph g) {
 		
-		 Layout<Integer, String> layout = new CircleLayout(g);
-		 layout.setSize(new Dimension(300,300)); 
-		 VisualizationViewer<Integer,String> vv = new VisualizationViewer<Integer,String>(layout);
-		 vv.setPreferredSize(new Dimension(350,350));
-		 
-		 vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+		menuBar = new JMenuBar();
+		menu = new JMenu("Zoom");
+		zoomIn = new JMenuItem("In");
+		zoomOut = new JMenuItem("Out");
+		menu.add(zoomIn);
+		menu.addSeparator();
+		menu.add(zoomOut);
 		
-		 DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
-		 vv.addKeyListener(gm.getModeKeyListener());
+		menuBar.add(menu);
 
-		 vv.setGraphMouse(gm);
-
-			final PickedState<Integer> pickedState = vv.getPickedVertexState();
-			pickedState.addItemListener(new ItemListener() {
-				@Override
-				public void itemStateChanged(ItemEvent e) {
+		this.setBounds(300,100,600,500);
+		this.setTitle("Grafo");
+		zoomIn.addActionListener(this);
+		zoomOut.addActionListener(this);
+		
+		Layout<Integer, String> layout = new CircleLayout(g);
+		layout.setSize(new Dimension(300,300)); 
+		VisualizationViewer<Integer,String> vv = new VisualizationViewer<Integer,String>(layout);
+		vv.setPreferredSize(new Dimension(350,350));
+		
+		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+		
+		DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+		vv.addKeyListener(gm.getModeKeyListener());
+	 
+		vv.setGraphMouse(gm);
+	
+		final PickedState<Integer> pickedState = vv.getPickedVertexState();
+		pickedState.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
 					Object subject = e.getItem();
-					String vertex = (String) subject;
-					System.out.println(vertex);
+					vertex = (String) subject;
 				}		
 			});
-			
-		 JFrame frame = new JFrame();	
-		 frame.getContentPane().add(vv);
-		 frame.pack();
-		 frame.setVisible(true); 
+
+		this.setLayout(new BorderLayout());
+		this.add(menuBar, BorderLayout.NORTH);
+		this.getContentPane().add(vv);
+		this.pack();
+		this.setVisible(true); 
 
     }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if(e.getSource() == zoomIn) {
+			System.out.println(vertex);
+		}
+		else if(e.getSource() == zoomOut) {
+			System.out.println(vertex);
+		}
+	}
 }
